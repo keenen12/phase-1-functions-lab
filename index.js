6c4d44ddc5773258ac1fb96ba5ce4cb6e7ad7607
Keenen Harral <kharral23@yahoo.com>
2022-11-10 18:25:38 -0800

    commit tree d7e5903b953f745bae615e04f85afa417c3efad1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(start) {
+  if (start > 42) {
+    return start - 42;
+  } else if (start < 42) {
+    return 42 - start;
+  }
+  
+}
+
+function distanceFromHqInFeet(start) {
+  return distanceFromHqInBlocks(start) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if(start > end) {
+    return (start - end) * 264;
+  } else if (start < end) {
+    return (end - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination)
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return (distance-400) *.02; 
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else if (distance > 2500) {
+    return 'cannot travel that far';
+
+  }
+
+}
+ 
+
